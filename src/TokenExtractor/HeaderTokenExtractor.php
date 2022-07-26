c80e426b75354d539e9ecde5851e1fac95a1a76f
Viktar Trukhanovich <v.trukhanovich@godeltech.com>
2022-07-26 16:07:43 +0300

    Changed HeaderTokenExtractor

--- src/TokenExtractor/HeaderTokenExtractor.php
+++ src/TokenExtractor/HeaderTokenExtractor.php
@@ -12,10 +12,6 @@ class HeaderTokenExtractor implements TokenExtractorInterface
     {
         $apiTokenHeader = strtolower(self::API_TOKEN_HEADER);
 
-        if (!$request->headers->has($apiTokenHeader)) {
-            return null;
-        }
-
         return $request->headers->get($apiTokenHeader);
     }
 }
